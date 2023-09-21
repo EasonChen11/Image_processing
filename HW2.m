3ce73b50a837d29130f7c044bdb8a3ac7291f70a
EasonChen <yschen1124@gmail.com>
2023-09-21 17:15:46 +0800

    正常版

--- HW2.m
+++ HW2.m
@@ -1,22 +1,30 @@
 clear
 close all
-i=imread("monkey.png");
+%read img
+i=imread("dog.png");
+D=[0 56;84 28];%dithering matrix
+%change to gray
 I=rgb2gray(i);
-subplot(2,2,1);
-imshow(I,gray(256));
-[M,N,C]=size(I);
-q=I/85;
-D=[0 56;84 28];
-r=repmat(D,floor(M/2),floor(N/2));
-q=q(1:2*floor(M/2),1:2*floor(N/2));
-D2=I-q*85;
-D3=D2>r;
-p=q+uint8(D3);
-subplot(2,2,2);
-imshow(p,gray(4)),colorbar
+%show gray picture
+subplot(2,2,1),imshow(I,gray(256)),title("gray"),colorbar
 
-DIt=q+uint8(r);
-A=floor(DIt/4);
-A=uint8(A*85);
-subplot(2,2,3);
-imshow(A,gray(4)),colorbar
+%quantization
+subplot(2,2,2),imshow(grayslice(I,4),gray(4)),title("quantization"),colorbar
+
+%Dithering2
+[M,N,C]=size(I);%get image size
+q=floor(I/85);%cut 4 path
+%extent matrix
+r=repmat(D,floor(M/2),floor(N/2));
+%modify matrix size
+q=q(1:2*floor(M/2),1:2*floor(N/2));
+Ichangesize=I(1:2*floor(M/2),1:2*floor(N/2));
+%binary thresholding with ditering map
+D2=(Ichangesize-q*85)>r;
+p=q+uint8(D2);
+subplot(2,2,3),imshow(p,gray(4)),title("Dithering method2"),colorbar
+
+%Dithering1
+DIt=I+uint8(r);
+A=floor(DIt/85);%cut 4 path
+subplot(2,2,4),imshow(A,gray(4)),title("Dithering method1"),colorbar
