30a98a3aedc4505058f673a94237529fffbcdc51
EasonChen <yschen1124@gmail.com>
2023-09-28 17:08:11 +0800

    HW3

--- HW3_1/HW3_1.m
+++ HW3_1/HW3_1.m
@@ -4,10 +4,10 @@ a=[0 18 150 255];
 b=[10 150 200 255];
 T=interp1(a,b,0:255,'linear');
 T_I=uint8(T(g+1));
-%adj=imadjust(g,[],[],2);
+adj=imadjust(g,[],[],0.5);
 figure,plot(T,'.'),axis tight
 figure,subplot(2,1,1),histogram(g),subplot(2,1,2),histogram(T_I)
-figure,imshowpair(g,T_I,'montage')
+figure,imshowpair(g,T_I,'montage'),title("Piecewise"),figure,imshow(adj),title("gamma")
 brisqueI=brisque(g);
 n=niqe(g);
 fprintf('BRISQUE score for original image is %0.4f.NIQEscore for original image is %0.4f.\n',brisqueI,n)
--- HW3_2/HW3_2.m
+++ HW3_2/HW3_2.m
@@ -1,13 +1,13 @@
 i=imread("EXP_4.bmp");
 g=rgb2gray(i);
-a=[0 150 190 255];
-b=[0 70 120 250];
+a=[0 100 190 230 255];
+b=[0 70 130 190 250];
 T=interp1(a,b,0:255,'linear');
 T_I=uint8(T(g+1));
-%adj=imadjust(g,[],[],2);
+adj=imadjust(g,[],[],2);
 figure,plot(T,'.'),axis tight
 figure,subplot(2,1,1),histogram(g),subplot(2,1,2),histogram(T_I)
-figure,imshowpair(g,T_I,'montage')
+figure,imshowpair(g,T_I,'montage'),title("Piecewise"),figure,imshow(adj),title("gamma")
 brisqueI=brisque(g);
 n=niqe(g);
 fprintf('BRISQUE score for original image is %0.4f.NIQEscore for original image is %0.4f.\n',brisqueI,n)
